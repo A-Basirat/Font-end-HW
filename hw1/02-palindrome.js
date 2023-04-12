d5680478b97b68181b8266bb90abd521544a67f3
Amir <a.basirat.67@gmail.com>
2023-04-12 16:57:00 -0700

    complete the Palindrome

--- hw1/02-palindrome.js
+++ hw1/02-palindrome.js
@@ -1,3 +1,32 @@
-const elem = document.querySelector('input');
+const inputNumber = document.querySelector("#input-number");
+const reslut = document.querySelector("#result");
 
-elem.addEventListener('input', handleInput);
+inputNumber.addEventListener("input", (event) => {
+  const { value } = event.target;
+
+  if (value < 0) {
+    updateResultMessage("Error: Please enter a positive number.", "red");
+  } else if (!value.trim() || isNaN(value)) {
+    updateResultMessage("Error: Please enter a valid number.", "red");
+  } else {
+    const isPalindrome = checkPalindrome(value);
+
+    if (isPalindrome) {
+      updateResultMessage("Yes, This is a palindrome.", "green");
+    } else {
+      updateResultMessage("No, Try again.", "red");
+    }
+  }
+});
+
+const checkPalindrome = (number) => {
+  const numberString = number.toString();
+  const reversedNumberString = numberString.split("").reverse().join("");
+
+  return numberString === reversedNumberString;
+};
+
+const updateResultMessage = (message, color) => {
+  result.textContent = message;
+  result.style.color = color;
+};
